e3a9c80bbc09a213927f1ec71cba32a20fb0a615
spiffycell <spiffycell@protonmail.com>
2022-03-17 09:07:07 -0400

    move over config for zshrc

--- setup.sh
+++ setup.sh
@@ -12,6 +12,9 @@ set -o pipefail
 echo '[*] Installing Ubuntu Packages'
 sudo apt-get install -y $(awk '{print $1}' packages.list)
 
+# move zshrc
+cp zshrc ~/.zshrc
+
 # install docker
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
